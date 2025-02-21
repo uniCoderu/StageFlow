87ed480c0611abc426e7c7a63255dd6c74c0eae0
monekeny <154050217+monekeny0@users.noreply.github.com>
2025-02-21 23:17:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,22 @@
 import sys
 sys.path.insert(0, '/content/drive/My Drive/StageFlow')
 
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, PreCheckoutQueryHandler
 from handlers.start_handler import start
 from handlers.menu_handler import menu_handler
 from handlers.text_handler import text_handler
-from handlers.payment_handler import successful_payment_handler
+from handlers.payment_handler import pre_checkout_handler, successful_payment_handler
+from handlers.marketplace_handler import marketplace_handler
 from config import API_KEY, logger
 
 async def main():
     application = ApplicationBuilder().token(API_KEY).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(menu_handler))
+    application.add_handler(CallbackQueryHandler(menu_handler, pattern="^(settings|payment_details|sbp|card|edit_payment_details|bank_|select_city|main_menu)$"))
+    application.add_handler(CallbackQueryHandler(marketplace_handler, pattern="^(marketplace|market_details_|buy_ticket_)$"))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
+    application.add_handler(PreCheckoutQueryHandler(pre_checkout_handler))
     application.add_handler(MessageHandler(filters.SUCCESSFUL_PAYMENT, successful_payment_handler))
 
     logger.info("Бот запущен и готов к работе.")
