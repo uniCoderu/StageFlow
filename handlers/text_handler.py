e7cf7a14425d2098ec2d373d3f628a9f74f3131d
monekeny <154050217+monekeny0@users.noreply.github.com>
2025-02-21 23:00:03 +0700

    Update text_handler.py

--- handlers/text_handler.py
+++ handlers/text_handler.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from storage.user_data import user_data
 from config import logger
-from handlers.menu_handler import show_settings_menu  # Импортируем вспомогательную функцию
+from handlers.menu_handler import show_settings_menu
 
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
@@ -28,7 +28,7 @@ async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         context.user_data["awaiting_card_number"] = False
 
         await update.message.reply_text("Ваши реквизиты сохранены! Возвращаю вас в меню настроек.")
-        await show_settings_menu(update, context)
+        await show_settings_menu(update, context)  # Используем тот же текст по умолчанию
 
     elif context.user_data.get("awaiting_city"):
         city = update.message.text
@@ -36,7 +36,7 @@ async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         context.user_data["awaiting_city"] = False
 
         await update.message.reply_text(f"Ваш город ({city}) сохранен! Возвращаю вас в меню настроек.")
-        await show_settings_menu(update, context)
+        await show_settings_menu(update, context)  # Используем тот же текст по умолчанию
         logger.info(f"Город {city} сохранен для пользователя {user_id}")
 
     else:
