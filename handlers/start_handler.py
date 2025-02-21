5a986f46597f2ff1c6842708e94ee6e0ea7f2ee5
monekeny <154050217+monekeny0@users.noreply.github.com>
2025-02-21 23:02:40 +0700

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -12,11 +12,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
     text = "Добро пожаловать! Я бот для безопасной перепродажи билетов на мероприятия.\nИспользуйте меню ниже для выбора нужного действия."
 
-    if update.message:  # Если это команда /start
+    if update.callback_query:  # Обработка callback-запроса (например, "Назад")
+        await update.callback_query.edit_message_text(text, reply_markup=reply_markup)
+        logger.info(f"Главное меню отображено для пользователя {update.callback_query.from_user.id} через callback")
+    elif update.message:  # Обработка команды /start
         await update.message.reply_text(text, reply_markup=reply_markup)
         logger.info(f"Команда /start выполнена для пользователя {update.message.from_user.id}")
-    elif update.callback_query:  # Если это вызов из callback (например, "Назад")
-        await update.callback_query.edit_message_text(text, reply_markup=reply_markup)
-        logger.info(f"Главное меню отображено для пользователя {update.callback_query.from_user.id}")
     else:
         logger.error("start вызван с некорректным update")
